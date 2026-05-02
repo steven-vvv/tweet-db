9b75c9cb4bc696f5366e6e0531ddd888115c9ae1
Steven Wu <76638078+steven-vvv@users.noreply.github.com>
2026-05-02 16:33:54 +0800

    perf(db): drop obsolete postgres user search indexes

---
+++ server/migrations/20260502000000_tantivy_search_indexes.sql
@@ -0,0 +1,3 @@
+DROP INDEX IF EXISTS tweet.idx_user_snapshot_user_name_recorded_at;
+DROP INDEX IF EXISTS tweet.idx_user_snapshot_user_name_prefix;
+DROP INDEX IF EXISTS tweet.idx_user_snapshot_display_name_prefix;
